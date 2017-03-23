51eb54e0afac0e586fa8faf49cbbed1713b3d940
einarzb <einarzb@gmail.com>
2017-03-23 12:28:55 +0200

    added yes no buttons and the calc of day or month

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -2,6 +2,7 @@ app.controller('mainCtrl', function($scope, beerService) {
 //toggling view
 $scope.intro = false;
 $scope.result = true;
+$scope.setAlert = true;
 $scope.alertStart = true;
 //intialize global vars
 $scope.total = 0;
@@ -14,9 +15,34 @@ $scope.calcBeer = function(fee){
   //toggle to hide #intro and reveal #result view
   $scope.intro = true;
   $scope.result = false;
+
   //calculate total
-  $scope.total = (fee / jemsCup);
+  $scope.total = Math.round((fee / jemsCup));
   console.log($scope.total);
+  $scope.dayAmount = Math.round(($scope.total / 30));
+  console.log($scope.dayAmount);
 };
+  //select
+  $scope.calander; //select ng-model
+  var month = $scope.total / 30;
+  var week = month / 5;
+  var day = week / 5;
+
+//yes/no buttons phase
+$scope.no = function(){
+  console.log("no");
+  alert("looooooser"); //maybe change to ng-hide of gif
+};
+
+$scope.yes = function(){
+  console.log("yes");
+  $scope.setAlert = false;
+  $scope.result = true;
+
+};
+
+
+
+
 
 });
