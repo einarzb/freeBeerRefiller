dec7c9d1491da22c75945cbf89073840b555204e
einarzb <einarzb@gmail.com>
2017-04-02 10:51:34 +0300

    added wework logo

---
+++ server.js
@@ -0,0 +1,12 @@
+//package and module requirements
+var express = require('express');
+var app = express();
+
+//middleware
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+
+
+app.listen(4000, function() {
+  console.log("beer free project. Listening on 4000.")
+});
