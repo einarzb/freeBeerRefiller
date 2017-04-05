e5a6cbf0f98e6f88e7136b8381528d0b7b659564
einarzb <einarzb@gmail.com>
2017-04-05 15:24:23 +0300

    weekly amount work if beer num bigger than 9

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -5,6 +5,8 @@ $scope.result = true;
 $scope.setAlert = true;
 $scope.alertStart = true;
 $scope.smallAmount = false;
+$scope.bigAmount = true;
+
 //intialize global vars
 $scope.total = 0;
 var jemsCup = 9;
@@ -26,17 +28,28 @@ $scope.calcBeer = function(fee){
       //calculate daily consumption in between (200 (min) - 2400)
       $scope.dayAmount = Math.round(($scope.total / 30));
       console.log($scope.dayAmount);
+      $scope.weekAmount =  Math.round(($scope.total / 7));
+      console.log($scope.weekAmount + "a week");
     }
      else {
       alert("please fill in an amount");
     };
-    //day amount
+
+    //day amount equal to 1 cup then its not plural
     if ($scope.dayAmount === 1 ) {
       console.log("1 cup a day");
       $scope.pluralSingle = "";
+    //if day amount is smaller then 0 cup per day then it shows only total
     } else if ($scope.dayAmount === 0) {
       $scope.smallAmount = true;
       console.log("small amount for daily suggestion");
+   } else if ($scope.dayAmount > 8) {
+      console.log("too much beers for one day");
+      $scope.pluralSingle = "s";
+      $scope.smallAmount = true;
+      $scope.bigAmount = false;
+
+    //if day amount bigger than 1 cup a day than it turn into cups
     } else {
       console.log("cups in plural");
       $scope.pluralSingle = "s";
